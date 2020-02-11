3cdd84c34c72236d2dd43a76eb86bd991509bbfc
harshityadav95 <harshityadav95@gmail.com>
2020-02-11 16:30:06 +0530

    client app

---
+++ Web Services/lab/clientapp/clientapp/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace clientapp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtregisterno = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnshow = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.regcheck = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtregisterno
+            // 
+            this.txtregisterno.Location = new System.Drawing.Point(110, 50);
+            this.txtregisterno.Name = "txtregisterno";
+            this.txtregisterno.Size = new System.Drawing.Size(100, 20);
+            this.txtregisterno.TabIndex = 0;
+            this.txtregisterno.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Register No";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnshow
+            // 
+            this.btnshow.Location = new System.Drawing.Point(122, 174);
+            this.btnshow.Name = "btnshow";
+            this.btnshow.Size = new System.Drawing.Size(75, 23);
+            this.btnshow.TabIndex = 2;
+            this.btnshow.Text = "Submit";
+            this.btnshow.UseVisualStyleBackColor = true;
+            this.btnshow.Click += new System.EventHandler(this.btnshow_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Label 2";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 121);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(110, 82);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 5;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(110, 121);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            // 
+            // regcheck
+            // 
+            this.regcheck.AutoSize = true;
+            this.regcheck.Location = new System.Drawing.Point(216, 50);
+            this.regcheck.Name = "regcheck";
+            this.regcheck.Size = new System.Drawing.Size(28, 13);
+            this.regcheck.TabIndex = 7;
+            this.regcheck.Text = "-------";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 230);
+            this.Controls.Add(this.regcheck);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnshow);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtregisterno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtregisterno;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnshow;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label regcheck;
+    }
+}
+
---
+++ Web Services/lab/clientapp/clientapp/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+
+namespace clientapp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnshow_Click(object sender, EventArgs e)
+        {
+            
+            regcheck.Text = txtregisterno.Text;
+            var client = new ServiceReference1.Service1Client();
+            ServiceReference1.GetResultResponse response=client.GetResult(txtregisterno.Text, 6, "ABC");
+           MessageBox.Show(response.registerNo + " " + response.grade);
+        }
+    }
+}
